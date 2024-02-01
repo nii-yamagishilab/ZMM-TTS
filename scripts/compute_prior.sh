636b4612c18b586c4efb16dd8f0f0090a9e01e05
gongchenghhu <45996835+gongchenghhu@users.noreply.github.com>
2024-02-01 22:36:34 +0800

    Delete scripts/compute_prior.sh

--- scripts/compute_prior.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=prior
-#SBATCH --out=prior.log
-#SBATCH --error=prior.error
-#SBATCH --time=70:00:00
-#SBATCH --cpus-per-task=16
-
-python prepare_data/compute_attention_prior.py
-
