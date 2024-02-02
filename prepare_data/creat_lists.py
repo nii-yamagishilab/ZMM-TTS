6acf2ced9bb4439e8dfa4e5a0b6c744ca36dccbc
gongchenghhu <45996835+gongchenghhu@users.noreply.github.com>
2024-02-02 10:11:35 +0800

    Update creat_lists.py

--- prepare_data/creat_lists.py
+++ prepare_data/creat_lists.py
@@ -1,6 +1,12 @@
+# ==============================================================================
+# Copyright (c) 2024, Yamagishi Laboratory, National Institute of Informatics
+# Author: Gong Cheng (gongchengcheng@tju.edu.cn)
+# All rights reserved.
+# ==============================================================================
+
 import os
 
-with open('Dataset/preprocessed_data/ZMM6/train.txt','r') as f1, open('Dataset/preprocessed_data/ZMM6/train_lists.txt','w') as f2:
+with open('Dataset/preprocessed_data/MM6/train.txt','r') as f1, open('Dataset/preprocessed_data/MM6/train_lists.txt','w') as f2:
       lines = f1.readlines()
       for line in lines:
           f2.write(line.split('|')[0]+'\n')
